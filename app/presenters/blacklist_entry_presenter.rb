f3f0c311a1e05effef459a7e443138d3fd8c8af7
Daniel Young <daniel.young@bluefrontier.co.uk>
2020-08-14 17:06:25 +0100

    feat: adding blacklist queue based updates

--- app/presenters/blacklist_entry_presenter.rb
+++ app/presenters/blacklist_entry_presenter.rb
@@ -4,6 +4,17 @@ class BlacklistEntryPresenter < BasePresenter
   attributes :id, :ip_literal, :ip_range,
              :path_literal, :path_regex, :headers
 
+  def self.record_for_microservices(record)
+    {
+        id: record.id,
+        ip_literal: record.ip_literal,
+        ip_range: record.ip_range,
+        path_literal: record.path_literal,
+        path_regex: record.path_regex,
+        headers: record.headers
+    }
+  end
+
   def self.collection_for_microservices(records)
     records.map do |record|
       {
