60758ac43432dee39b5072e578b59335c86c958e
Jack Regnart <Jack.Regnart@bluefrontier.co.uk>
2020-11-05 14:49:21 +0000

    fix: audit_logs#show query

--- spec/controllers/api/audit_logs_controller_spec.rb
+++ spec/controllers/api/audit_logs_controller_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::AuditLogsController, type: :controller do
   describe 'GET #index' do
-    context 'returns a success response' do
+    context 'response' do
       before do
         get(:index)
       end
@@ -14,14 +14,41 @@ RSpec.describe Api::V1::AuditLogsController, type: :controller do
   end
 
   describe 'GET #show' do
-    context 'returns a success response' do
-      let(:audit) { Audited::Audit.first }
+    let(:user) { User.first }
+    let(:audit) { user.audits.first }
 
-      before do
-        get(:show, params: { id: audit.id })
-      end
+    before do
+      get(:show, params: { id: audit.id })
+    end
 
+    let(:json_body) { JSON.parse(response.body) }
+
+    context 'response' do
       it { expect(response).to be_successful }
+    end
+
+    context '#auditable_id' do
+      it { expect(json_body['auditable_id']).to eq(user.id) }
+    end
+
+    context '#auditable_type' do
+      it { expect(json_body['auditable_type']).to eq('User') }
+    end
+
+    context '#associated_id' do
+      it { expect(json_body['associated_id']).to eq(user.account.id) }
+    end
+
+    context '#associated_type' do
+      it { expect(json_body['associated_type']).to eq('Account') }
+    end
+
+    context '#action' do
+      it { expect(json_body['action']).to eq('create') }
+    end
+
+    context '#version' do
+      it { expect(json_body['version']).to eq(1) }
     end
   end
 end
