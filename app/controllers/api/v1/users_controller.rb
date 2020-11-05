29ec9227973387e1e09e0f4a36ce0d0b58257fe8
Jack Regnart <Jack.Regnart@bluefrontier.co.uk>
2020-11-05 10:54:36 +0000

    feat: abstract restful resources & fix audit_log default filter

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,72 +2,34 @@
 
 module Api
   module V1
-    class UsersController < ApplicationController
-      before_action :find_user
-
+    # Users controller
+    # REST actions inherited from API::V1::BaseController
+    class UsersController < Api::V1::BaseController
       # GET /api/v1/users
       def index
-        @users = UserFilter.records(params, scope: current_account)
+        @records = UserFilter.records(params, scope: current_account)
 
         render json:
-          UserPresenter.collection(@users, params[:page]), status: :ok
-      end
-
-      # GET /api/v1/users/:id
-      def show
-        render json: @user.to_json(only: %i[id email time_zone created_at updated_at]), status: :ok
-      end
-
-      # POST /api/v1/users
-      def create
-        @user = current_account.users.build(user_params)
-
-        if @user.save
-          render json:
-            @user.to_json(only: %i[id email time_zone created_at updated_at]), status: :created
-        else
-          render json: @user.errors,
-                 status: :unprocessable_entity
-        end
-      end
-
-      # PUT /api/v1/users/:id
-      def update
-        if @user.update(user_params)
-          render json: @user, status: :ok
-        else
-          render json: @user.errors,
-                 status: :unprocessable_entity
-        end
-      end
-
-      # DELETE /api/v1/users/:id
-      def destroy
-        if @user.destroy
-          render nothing: true, status: :no_content
-        else
-          render nothing: true, status: :unprocessable_entity
-        end
-      end
-
-      # GET /api/v1/users/:id/audit_logs
-      def audit_logs
-        @audit_logs =
-          AuditableFilter.records(params, scope: current_account)
-
-        render json:
-          AuditablePresenter.collection(@audit_logs, params[:page]), status: :ok
+          UserPresenter.collection(@records, params[:page]), status: :ok
       end
 
       private
 
-      def find_user
-        return if params[:id].blank?
-
-        @user = current_account.users.find(params[:id])
+      def record_association
+        :users
       end
 
-      def user_params
+      def record_json_attributes
+        { only: %i[id email time_zone created_at updated_at] }
+      end
+
+      def set_audit_logs_filter
+        params[:filter] ||= {}
+        params[:filter][:auditable_id] = params[:id]
+        params[:filter][:auditable_type] = 'User'
+      end
+
+      def record_params
         params.fetch(:user).permit(
           :email,
           :password,
