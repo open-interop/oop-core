592e98283ae596dfc97ac630a6079daa503a4d97
Jack Regnart <jack.regnart@bluefrontier.co.uk>
2023-12-13 20:41:56 +0000

    feat: fix tests

--- spec/factories/tempr_templates.rb
+++ spec/factories/tempr_templates.rb
@@ -3,22 +3,24 @@
 FactoryBot.define do
   factory :tempr_template do
     temprs do
-      {
-        language: 'js',
-        script:
-          'const http = require("http");
-          const ret = [];
-          for (const res of message.body) {
-              ret.push(http(
-                  "POST",
-                  "https",
-                  "test.co.uk",
-                  "/post/path"
-                  { id: result.id, value: result.val }
-              ));
-          }
-          module.exports = ret;'
-      }
+      [
+        {
+          language: 'js',
+          script:
+            'const http = require("http");
+            const ret = [];
+            for (const res of message.body) {
+                ret.push(http(
+                    "POST",
+                    "https",
+                    "test.co.uk",
+                    "/post/path"
+                    { id: result.id, value: result.val }
+                ));
+            }
+            module.exports = ret;'
+        }
+      ]
     end
   end
 end
